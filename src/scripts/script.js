8188a355da682e21cb3022a55140d29498ba8e9a
yasser8111 <yasserxd653@gmail.com>
2025-10-06 14:13:10 +0300

    fix

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -71,14 +71,24 @@ import {
 } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "VITE_FIREBASE_API_KEY",
-  authDomain: "VITE_FIREBASE_AUTH_DOMAIN",
-  projectId: "VITE_FIREBASE_PROJECT_ID",
-  storageBucket: "VITE_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "VITE_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "VITE_FIREBASE_APP_ID",
-  measurementId: "VITE_FIREBASE_MEASUREMENT_ID"
+  apiKey: "AIzaSyBoXoSi376ZtbrIDc0Oa4_yf8-tJKcHV-4",
+  authDomain: "database-nextstore.firebaseapp.com",
+  projectId: "database-nextstore",
+  storageBucket: "database-nextstore.firebasestorage.app",
+  messagingSenderId: "463214736059",
+  appId: "1:463214736059:web:d882da8f1e06968eedef9e",
+  measurementId: "G-M6M6KZ1W74",
 };
+
+// const firebaseConfig = {
+//   apiKey: "VITE_FIREBASE_API_KEY",
+//   authDomain: "VITE_FIREBASE_AUTH_DOMAIN",
+//   projectId: "VITE_FIREBASE_PROJECT_ID",
+//   storageBucket: "VITE_FIREBASE_STORAGE_BUCKET",
+//   messagingSenderId: "VITE_FIREBASE_MESSAGING_SENDER_ID",
+//   appId: "VITE_FIREBASE_APP_ID",
+//   measurementId: "VITE_FIREBASE_MEASUREMENT_ID"
+// };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);

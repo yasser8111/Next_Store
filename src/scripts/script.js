45180dafb46968b5f652756a5b38bbb1c2dd3870
yasser8111 <yasserxd653@gmail.com>
2025-10-06 14:31:45 +0300

    fix vite problom

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -124,8 +124,7 @@ async function loadProducts() {
         window.location.href = `product-details.html?id=${p.id}`;
       };
 
-      // ✅ إضافة مسار src/assets/ تلقائياً
-      const imagePath = `./src/assets/img/t-shirt/${p.images[0]}`;
+      const imagePath = `./public/img/t-shirt/${p.images[0]}`;
 
       card.innerHTML = `
     <img src="${imagePath}" alt="${p.name}" loading="lazy">

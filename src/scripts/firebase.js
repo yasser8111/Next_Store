69f6ec97ce59741652742df425148fe1dba3d2b6
yasser8111 <yasserxd653@gmail.com>
2025-10-07 14:41:42 +0300

    fix firebase problom

--- src/scripts/firebase.js
+++ src/scripts/firebase.js
@@ -1,18 +1,9 @@
-// firebase.js
+import { firebaseConfig } from "./config.js"; // تأكد من المسار الصحيح
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore, collection, getDocs, limit, query } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
-};
 
 export async function getAllProducts() {
   const q = query(collection(db, "products"), limit(6));

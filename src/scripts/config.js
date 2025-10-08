be070dcef200a53058018013ae1d2dabf1dad8b7
yasser8111 <yasserxd653@gmail.com>
2025-10-08 17:10:09 +0300

    fix bugs

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,9 +1,8 @@
-// js/config.js
-export const firebaseConfig = {
-  apiKey: window.env?.FIREBASE_API_KEY || "AIzaSyBoXoSi376ZtbrIDc0Oa4_yf8-tJKcHV-4",
-  authDomain: window.env?.FIREBASE_AUTH_DOMAIN || "database-nextstore.firebaseapp.com",
-  projectId: window.env?.FIREBASE_PROJECT_ID || "database-nextstore",
-  storageBucket: window.env?.FIREBASE_STORAGE_BUCKET || "database-nextstore.appspot.com",
-  messagingSenderId: window.env?.FIREBASE_MESSAGING_SENDER_ID || "463214736059",
-  appId: window.env?.FIREBASE_APP_ID || "1:463214736059:web:d882da8f1e06968eedef9e",
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };

6d9103e5e2ad328350c9097f7b50b0d1831d190b
yasser8111 <yasserxd653@gmail.com>
2025-10-08 17:05:34 +0300

    fix bugs

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,9 +1,9 @@
-// config.js
+// js/config.js
 export const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: window.env?.FIREBASE_API_KEY || "AIzaSyBoXoSi376ZtbrIDc0Oa4_yf8-tJKcHV-4",
+  authDomain: window.env?.FIREBASE_AUTH_DOMAIN || "database-nextstore.firebaseapp.com",
+  projectId: window.env?.FIREBASE_PROJECT_ID || "database-nextstore",
+  storageBucket: window.env?.FIREBASE_STORAGE_BUCKET || "database-nextstore.appspot.com",
+  messagingSenderId: window.env?.FIREBASE_MESSAGING_SENDER_ID || "463214736059",
+  appId: window.env?.FIREBASE_APP_ID || "1:463214736059:web:d882da8f1e06968eedef9e",
 };

2dd16ce4d2d17c6e6e63a5698b5deaa94bc9c34f
yasser8111 <yasserxd653@gmail.com>
2025-10-08 17:01:36 +0300

    fix bugs

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,5 +1,5 @@
 // config.js
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,

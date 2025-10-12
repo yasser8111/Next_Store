d918c60a18148b4eb711f6ce037e4d970713f02d
yasser8111 <yasserxd653@gmail.com>
2025-10-12 09:59:41 +0300

    fix

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -8,25 +8,14 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
-// ✅ إعدادات Firebase الخاصة بك
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBoXoSi376ZtbrIDc0Oa4_yf8-tJKcHV-4",
-//   authDomain: "database-nextstore.firebaseapp.com",
-//   projectId: "database-nextstore",
-//   storageBucket: "database-nextstore.appspot.com",
-//   messagingSenderId: "463214736059",
-//   appId: "1:463214736059:web:d882da8f1e06968eedef9e",
-//   measurementId: "G-M6M6KZ1W74",
-// };
-
 const firebaseConfig = {
-  apikey: prosses.env.FIREBASE_API_KEY,
-  authDomain: prosses.env.FIREBASE_AUTH_DOMAIN,
-  projectId: prosses.env.FIREBASE_PROJECT_ID,
-  storageBucket: prosses.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: prosses.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: prosses.env.FIREBASE_APP_ID,
-  measurementId: prosses.env.FIREBASE_MEASUREMENT_ID,
+  apikey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // ✅ تهيئة Firebase

12977301bd2286e412a441193a61c15748611b9f
yasser8111 <yasserxd653@gmail.com>
2025-10-18 14:51:27 +0300

    fix bugs

--- server.js
+++ server.js
@@ -3,16 +3,20 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
-const port = 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// خدم الملفات الثابتة من مجلد public
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+// جميع الطلبات تخدم index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+// تصدير التطبيق لـ Vercel
+export default app;
 
 app.listen(port, () => {
   console.log(`--> http://localhost:${port}`);
